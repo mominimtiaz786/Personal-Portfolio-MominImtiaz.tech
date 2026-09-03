2aad396b90da9a625e05625c3abb237a29bc02dd
Momin Imtiaz <mominimtiaz0786@gmail.com>
2026-09-03 18:46:46 +0500

    Certifications Updated | added AI Digiskills

--- src/app/data/certifications.data.ts
+++ src/app/data/certifications.data.ts
@@ -2,6 +2,26 @@ import { Certification } from '../models/certification.model';
 
 export const CERTIFICATIONS: Certification[] = [
   { 
+    title: 'Associate AI Engineer for Developers', 
+    issuer: 'DataCamp',
+    verification_link: 'https://www.datacamp.com/statement-of-accomplishment/track/24d0badc5b36f5f841071450d14387b52b229a2e' 
+  },
+  { 
+    title: 'Developing AI Applications', 
+    issuer: 'DataCamp',
+    verification_link: 'https://www.datacamp.com/statement-of-accomplishment/track/57181dd5b05806e2a17faaca2277f191d4c110e2' 
+  },
+  { 
+    title: 'OpenAI Fundamentals', 
+    issuer: 'DataCamp',
+    verification_link: 'https://www.datacamp.com/statement-of-accomplishment/track/4c80a78a466c8f577b84abd5a5a4bcbef5ea78eb' 
+  },
+  { 
+    title: 'Artificial Intelligence Using Python', 
+    issuer: 'DigiSkills.pk',
+    verification_link: 'https://digiskills.pk/verify/XNDV4GFMK' 
+  },
+  { 
     title: 'Full Stack Web Development with Angular', 
     issuer: 'Hong Kong University of Science and Technology (Coursera)',
     verification_link: 'https://www.coursera.org/account/accomplishments/specialization/6EXHJYK88LYE' 
@@ -30,20 +50,5 @@ export const CERTIFICATIONS: Certification[] = [
     title: 'Developing and Deploying a Complete Django Web Application',
     issuer: 'University of Michigan (Coursera)',
     verification_link: 'https://www.coursera.org/account/accomplishments/verify/TGBNL7CA9AFB'
-  },
-  { 
-    title: 'Associate AI Engineer for Developers', 
-    issuer: 'DataCamp',
-    verification_link: 'https://www.datacamp.com/statement-of-accomplishment/track/24d0badc5b36f5f841071450d14387b52b229a2e' 
-  },
-  { 
-    title: 'Developing AI Applications', 
-    issuer: 'DataCamp',
-    verification_link: 'https://www.datacamp.com/statement-of-accomplishment/track/57181dd5b05806e2a17faaca2277f191d4c110e2' 
-  },
-  { 
-    title: 'OpenAI Fundamentals', 
-    issuer: 'DataCamp',
-    verification_link: 'https://www.datacamp.com/statement-of-accomplishment/track/4c80a78a466c8f577b84abd5a5a4bcbef5ea78eb' 
   },
 ];
